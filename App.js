e14ebc85c3bf0df04d296a0f66145cfbe8dfa4d9
LuizGustavoDiniz <gustavo.diniz.gon@gmail.com>
2025-07-31 16:50:51 -0300

    criacao do botao de adcionar pessoas

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
 
 const App = () => {
   return(
@@ -7,7 +7,9 @@ const App = () => {
       <Text>Contador de Clientes</Text>
 
       <View>
-        
+          <TouchableOpacity>
+            <Text></Text>
+          </TouchableOpacity>
       </View>
     </View>
   )
