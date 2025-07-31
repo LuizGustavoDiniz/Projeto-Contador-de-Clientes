74642d225a72ccd2c2dabe3755a307a3b5dff4f1
LuizGustavoDiniz <gustavo.diniz.gon@gmail.com>
2025-07-31 20:31:56 -0300

    adicao de estilo em todo o projeto

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
 
 //git add . pra mandar todos os arquivos alterados pra stage prontos para o commit
@@ -6,15 +6,19 @@ import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
 const App = () => {
   return(
      <View style={styles.container}>
-      <Text>Pessoas no restaurante</Text>
+      <Text style={styles.title}>Pessoas no restaurante</Text>
 
-      <View>
-        <Text>0</Text>
+      <View style={styles.counterArea}>
+        <Text style={styles.counterValue}>10</Text>
       </View>
 
-      <View>
-          <TouchableOpacity>
-            <Text>Adicionar</Text>
+      <View style={styles.btnArea}>
+          <TouchableOpacity style={styles.btn}>
+            <Text style={styles.btnText}>Adicionar</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.btn}>
+            <Text style={styles.btnText}>Remover</Text>
           </TouchableOpacity>
       </View>
     </View>
@@ -26,6 +30,41 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  title: {
+    fontSize: 20,
+    color: '#000',
+    fontWeight: 'bold'
+  },
+  counterArea:{
+    backgroundColor: '#222',
+    width: 50,
+    height: 50,
+    borderRadius: 5,
+    marginVertical: 20,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  counterValue:{
+    color: '#FFF',
+    fontSize: 20
+  },
+  btnArea: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '50%'
+  },
+  btn:{
+    backgroundColor: '#0088ff',
+    height: 40,
+    paddingHorizontal: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+  },
+  btnText: {
+    color: '#fff',
+    fontSize: 16
   }
 })
 
